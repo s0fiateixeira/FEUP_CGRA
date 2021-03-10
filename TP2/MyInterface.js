893e9a1f0fe5af351188988165fe5bae007a822c
Sofia Teixeira <up201806629@fe.up.pt>
2021-03-10 20:42:14 +0000

    Preparing TP2

---
+++ TP2/MyInterface.js
@@ -0,0 +1,33 @@
+import {CGFinterface, dat} from '../lib/CGF.js';
+
+/**
+* MyInterface
+* @constructor
+*/
+export class MyInterface extends CGFinterface {
+    constructor() {
+        super();
+    }
+
+    init(application) {
+        // call CGFinterface init
+        super.init(application);
+        
+        // init GUI. For more information on the methods, check:
+        // https://github.com/dataarts/dat.gui/blob/master/API.md
+        this.gui = new dat.GUI();
+
+        //Checkbox element in GUI
+        this.gui.add(this.scene, 'displayAxis').name('Display Axis');
+        this.gui.add(this.scene, 'showDiamond').name('Show Diamond');
+        this.gui.add(this.scene, 'showTriangle').name('Show Triangle');
+        this.gui.add(this.scene, 'showParallelogram').name('Show Parallelogram');
+        this.gui.add(this.scene, 'showTriangleSmall').name('Show Small Triangle');
+        this.gui.add(this.scene, 'showTriangleBig').name('Show Big Triangle');
+
+        //Slider element in GUI
+        this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
+
+        return true;
+    }
+}
