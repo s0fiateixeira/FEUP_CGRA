f57819bf16c8ccebb2005ab2745650b63dec2fd3
Sofia Teixeira <up201806629@fe.up.pt>
2021-03-11 15:38:50 +0000

    TP3 - Exercise 4

--- TP3/MyScene.js
+++ TP3/MyScene.js
@@ -97,7 +97,6 @@ export class MyScene extends CGFscene {
         this.customMaterial.setAmbient(...this.hexToRgbA(this.customMaterialValues['Ambient']));
         this.customMaterial.setDiffuse(...this.hexToRgbA(this.customMaterialValues['Diffuse']));
         this.customMaterial.setSpecular(...this.hexToRgbA(this.customMaterialValues['Specular']));
-
         this.customMaterial.setShininess(this.customMaterialValues['Shininess']);
 
     };
@@ -129,6 +128,13 @@ export class MyScene extends CGFscene {
         this.material3.setSpecular(1, 0, 0, 1.0);
         this.material3.setShininess(10.0);
 
+        // Wood (no diffuse, no specular)
+        this.material4 = new CGFappearance(this);
+        this.material4.setAmbient(0, 0, 0, 1.0);
+        this.material4.setDiffuse(102/255, 51/255, 0, 1.0);
+        this.material4.setSpecular(0.1, 0.1, 0.1, 1.0);
+        this.material4.setShininess(10.0);
+
         // Custom material (can be changed in the interface)
         // initially midrange values on ambient, diffuse and specular, on R, G and B respectively
 
@@ -142,10 +148,10 @@ export class MyScene extends CGFscene {
 
         this.updateCustomMaterial();
 
-        this.materials = [this.material1, this.material2, this.material3, this.customMaterial];
+        this.materials = [this.material1, this.material2, this.material3, this.material4, this.customMaterial];
 
         // Labels and ID's for object selection on MyInterface
-        this.materialIDs = {'Red Ambient': 0, 'Red Diffuse': 1, 'Red Specular': 2, 'Custom': 3 };
+        this.materialIDs = {'Red Ambient': 0, 'Red Diffuse': 1, 'Red Specular': 2, 'Wood' : 3, 'Custom': 4 };
     }
     display() {
         // ---- BEGIN Background, camera and axis setup
